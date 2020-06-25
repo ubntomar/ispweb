5d584ebd0be11a20bd14f810dc210a98c13744bf
ubntomar <omar_alberto_h@yahoo.es>
2020-06-25 12:48:36 -0500

    vue code

---
+++ devicePingResponse.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+if ( !isset($_SESSION['login']) || $_SESSION['login'] !== true) 
+		{
+		header('Location: login/index.php');
+		exit;
+		}
+else    {
+		$user=$_SESSION['username'];
+		}
+include("PingTime.php");
+
+
+?>
