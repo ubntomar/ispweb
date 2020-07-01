b8654825f203c4cb29da5376050e5c7983f3cfa5
Omar Hernandez <omar_alberto_h@yahoo.es>
2020-07-01 00:37:47 -0500

    adding user

--- login/index.php
+++ login/index.php
@@ -2,181 +2,184 @@
 session_start();
 $urlprev=$_SESSION['urlsource'];
 session_destroy();
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
+
 <head>
-    <title>Isp Experts -Administrador ISP -Solucion sencilla  para tu negocio </title>
+    <title>Isp Experts -Administrador ISP -Solucion sencilla para tu negocio </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	 
+
     <!-- Bootstrap -->
-     <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link href="css/style.css" rel="stylesheet" media="screen">
     <link href="css/bootstrap-social.css" rel="stylesheet" media="screen">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:300,400,500" rel="stylesheet">
-    
+
 
 </head>
 
 <body>
 
-	
-		<div class="row">
-                    <div class="columna col bg-dark">
-                        
-                        <div class=" nuevo_contenido">
-                            
-                            <!-- inicio de contenido de pagina -->
-                             <!-- Form Login -->
-                            <div>
-                                <div class="logo text-light" >
-                                     <h2><?php include('db.php'); echo $logotxt; ?></h2>
-                                </div>
-                                <form class="form-horizontal" id="login_form">
-                                     <h2>Bienvenido</h2>
 
-                                    <div class="line"></div>
-                                    <div class="form-group">
-                                        <input type="text" id="inputEmail" name="username" placeholder="Username">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" id="inputPassword" name="password" placeholder="Password">
-                                    </div> 
-                                    <a class="forgotten-password-link" href="forgot_form.php">Forgot password?</a>
-                                    <a href="registration_form.php" class="btn btn-lg btn-register">Register</a>
+    <div class="row">
+        <div class="columna col bg-dark">
 
-                                    <button id="signin" type="submit" class="btn btn-lg btn-primary btn-sign-in"
-                                    data-loading-text="Loading...">Sign in</button>
-                                    <div class="messagebox">
-                                        <div id="alert-message"></div>
-                                    </div>
-                                    <div class="social">
-                                        <a href="twitter_connect.php"><img src="img/twitter.png"/></a>
-                                        <a href="facebook_connect.php"><img src="img/fb.png"/></a>
-                                        <a href="google_connect.php?=code"><img src="img/gplus.png"/></a>
-                                    
-                                    </div>
-                                </form>
-                            </div>
-                            <!-- fin de contenido de pagina --> 
-                        </div>
+            <div class=" nuevo_contenido">
+
+                <!-- inicio de contenido de pagina -->
+                <!-- Form Login -->
+                <div>
+                    <div class="logo text-light">
+                        <h2><?php include('db.php'); echo $logotxt; ?></h2>
                     </div>
-        </div>
-	
+                    <form class="form-horizontal" id="login_form">
+                        <h2>Bienvenido</h2>
 
-    
-   
-	
+                        <div class="line"></div>
+                        <div class="form-group">
+                            <input type="text" id="inputEmail" name="username" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" id="inputPassword" name="password" placeholder="Password">
+                        </div>
+                        <a class="forgotten-password-link" href="forgot_form.php">Forgot password?</a>
+                        <a href="registration_form.php" class="btn btn-lg btn-register">Register</a>
+
+                        <button id="signin" type="submit" class="btn btn-lg btn-primary btn-sign-in"
+                            data-loading-text="Loading...">Sign in</button>
+                        <div class="messagebox">
+                            <div id="alert-message"></div>
+                        </div>
+                        <div class="social">
+                            <a href="twitter_connect.php"><img src="img/twitter.png" /></a>
+                            <a href="facebook_connect.php"><img src="img/fb.png" /></a>
+                            <a href="google_connect.php?=code"><img src="img/gplus.png" /></a>
+
+                        </div>
+                    </form>
+                </div>
+                <!-- fin de contenido de pagina -->
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 
 
     <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="../bower_components/Popper/popper.min.js" ></script>  
-    <script src="../bower_components/bootstrap/dist/js/bootstrap.js"></script> 
+    <script src="../bower_components/Popper/popper.min.js"></script>
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.js"></script>
     <script type="text/javascript" src="js/jquery.validate.js"></script>
 
-	<script type="text/javascript">
-      (function() {
-       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-       po.src = 'https://apis.google.com/js/client:plusone.js';
-       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-     })();
+    <script type="text/javascript">
+    (function() {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/client:plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
     </script>
- <script>
-        $(document).ready(function() {
-		jQuery.validator.addMethod("noSpace", function(value, element) { 
-     return value.indexOf(" ") < 0 && value != ""; 
-  }, "Spaces are not allowed");
+    <script>
+    $(document).ready(function() {
+        jQuery.validator.addMethod("noSpace", function(value, element) {
+            return value.indexOf(" ") < 0 && value != "";
+        }, "Spaces are not allowed");
 
-  
-  //add validator
-$("#login_form").validate({
-onfocusout: false,
-    onkeyup: false,
-    onclick: false,
-					rules: {
-                        username: {
-                            required: true,
-							noSpace: true
-                        },
-                        password: {
-                            required: true,
-                            minlength: 6
+
+        //add validator
+        $("#login_form").validate({
+            onfocusout: false,
+            onkeyup: false,
+            onclick: false,
+            rules: {
+                username: {
+                    required: true,
+                    noSpace: true
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                }
+            },
+
+            messages: {
+                username: {
+                    required: "Enter your username"
+                },
+                password: {
+                    required: "Enter your password",
+                    minlength: "Password must be minimum 6 characters"
+                },
+            },
+
+
+
+            errorPlacement: function(error, element) {
+                error.hide();
+                $('.messagebox').hide();
+                error.appendTo($('#alert-message'));
+                $('.messagebox').slideDown('slow');
+
+
+
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).parents('.form-group').addClass('has-error');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).parents('.form-group').removeClass('has-error');
+                $(element).parents('.form-group').addClass('has-success');
+            }
+        });
+
+        $("#login_form").submit(function() {
+            $('.social').hide('slow');
+            if ($("#login_form").valid()) {
+                $('.messagebox').slideUp('slow');
+                var data1 = $('#login_form').serialize();
+                $("button").button('loading');
+                $.ajax({
+                    type: "POST",
+                    url: "login.php",
+                    data: data1,
+                    dataType: 'json',
+                    success: function(msg) {
+                        if (msg.result == 1) {
+                            $('.messagebox').addClass("success-message");
+                            $('.message').slideDown('slow');
+                            $('#alert-message').text("Logged in.. Redirecting");
+
+                            $('#login_form').fadeOut(5000);
+                            //$("button").button('reset');
+                            window.location = "members.php?urlprev=<?php echo $urlprev; ?>";
+                        } else {
+                            $("button").button('reset');
+                            console.log(msg.result);
+                            $('.messagebox').hide();
+                            $('.messagebox').addClass("error-message");
+                            $('#alert-message').html(msg.result);
+                            $('.messagebox').slideDown('slow');
                         }
                     },
-
-                    messages: {
-                        username: {
-                            required: "Enter your username"
-                                                    },
-                        password: {
-                            required: "Enter your password",
-                            minlength: "Password must be minimum 6 characters"
-                        },
-                    },
-
-
-
-                    errorPlacement: function(error, element) {
-                        error.hide();
-						$('.messagebox').hide();
-                        error.appendTo($('#alert-message'));
+                    error: function(msg) {
+                        $("button").button('reset');
+                        $('.messagebox').hide();
+                        $('.messagebox').addClass("error-message");
+                        $('#alert-message').html(msg.result);
                         $('.messagebox').slideDown('slow');
-                       
-						
-						
-                    },
-                    highlight: function(element, errorClass, validClass) {
-                        $(element).parents('.form-group').addClass('has-error');
-                    },
-                    unhighlight: function(element, errorClass, validClass) {
-                        $(element).parents('.form-group').removeClass('has-error');
-                        $(element).parents('.form-group').addClass('has-success');
                     }
                 });
-				
-            $("#login_form").submit(function() {
-				$('.social').hide('slow');
-                if ($("#login_form").valid()) {
-				$('.messagebox').slideUp('slow');
-                    var data1 = $('#login_form').serialize();
-					$("button").button('loading');
-                    $.ajax({
-                        type: "POST",
-                        url: "login.php",
-                        data: data1,
-						dataType: 'json',
-                        success: function(msg) {						
-                            if (msg.result == 1) {
-							$('.messagebox').addClass("success-message");
-							$('.message').slideDown('slow');
-							$('#alert-message').text("Logged in.. Redirecting");
-							
-                                $('#login_form').fadeOut(5000);
-								//$("button").button('reset');
-                                window.location = "members.php?urlprev=<?php echo $urlprev; ?>";
-                            } else {	
-							$("button").button('reset');
-							console.log(msg.result);
-							$('.messagebox').hide();
-							$('.messagebox').addClass("error-message");
-							$('#alert-message').html(msg.result);
-							 $('.messagebox').slideDown('slow');
-							}
-                        },
-						error: function(msg){
-							$("button").button('reset');
-							$('.messagebox').hide();
-							$('.messagebox').addClass("error-message");
-							$('#alert-message').html(msg.result);
-							 $('.messagebox').slideDown('slow');
-						}
-                    });
-                }
-                return false;
-            });
-        
+            }
+            return false;
         });
-		
 
+    });
     </script>
 </body>
 
--- login/registration_form.php
+++ login/registration_form.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <head>
     <title>PHP Login System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,17 +7,18 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="css/style.css" rel="stylesheet" media="screen">
 </head>
+
 <body>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery.validate.js"></script>
     <div class="logo">
-         <h2><?php include 'db.php';
+        <h2><?php include 'db.php';
 echo $logotxt;?></h2>
 
     </div>
     <form class="form-horizontal" id="register_form" method="post">
-         <h2>Register</h2>
+        <h2>Register</h2>
 
         <div class="line"></div>
         <div class="form-group">
@@ -32,111 +34,110 @@ echo $logotxt;?></h2>
             <input type="password" id="inputPassword_2" name="retype_password" placeholder="Retype Password">
         </div>
 
-<button disabled type="submit"
-        class="btn btn-lg btn-primary btn-sign-in" data-loading-text="Loading...">Register</button>
-        	<a href="index.php"
-        class="btn btn-lg btn-register">Sign in</a>
+        <button disabled type="submit" class="btn btn-lg btn-primary btn-sign-in"
+            data-loading-text="Loading...">Register</button>
+        <a href="index.php" class="btn btn-lg btn-register">Sign in</a>
         <div class="messagebox">
             <div id="alert-message"></div>
         </div>
     </form>
     <script>
-        $(document).ready(function() {
+    $(document).ready(function() {
 
-		jQuery.validator.addMethod("noSpace", function(value, element) {
-     return value.indexOf(" ") < 0 && value != "";
-  }, "Spaces are not allowed");
-/*jQuery.validator.addMethod("maxlength", function (value, element, param) {
-    console.log('element= ' + $(element).attr('name') + ' param= ' + param )
-    if ($(element).val().length > param) {
-        return false;
-    } else {
-		console.log($(element).val().length);
-        return true;
-    }
-}, "You have reached the maximum number of characters allowed for this field.");
-*/
+        jQuery.validator.addMethod("noSpace", function(value, element) {
+            return value.indexOf(" ") < 0 && value != "";
+        }, "Spaces are not allowed");
+        /*jQuery.validator.addMethod("maxlength", function (value, element, param) {
+            console.log('element= ' + $(element).attr('name') + ' param= ' + param )
+            if ($(element).val().length > param) {
+                return false;
+            } else {
+        		console.log($(element).val().length);
+                return true;
+            }
+        }, "You have reached the maximum number of characters allowed for this field.");
+        */
 
-            $("#register_form").submit(function() {
+        $("#register_form").submit(function() {
 
-                $("#register_form").validate({
-                    rules: {
-                        email: {
-                            required: true,
-                            email: true
-                        },
-                        username: {
-                            required: true,
-							noSpace: true
-                        },
-                        password: {
-                            required: true,
-                            minlength: 6
-							//maxlength: 8
-                        },
-                        retype_password: {
-                            required: true,
-                            equalTo: "#inputPassword"
-                        },
+            $("#register_form").validate({
+                rules: {
+                    email: {
+                        required: true,
+                        email: true
                     },
-                    messages: {
-                        email: {
-                            required: "Enter your email address",
-                            email: "Enter valid email address"
-                        },
-                        username: {
-                            required: "Enter Username",
-
-                        },
-                        password: {
-                            required: "Enter your password",
-                            minlength: "Password must be minimum 6 characters"
-							//maxlength: "Password must be maximum 8 characters"
-
-                        },
-                        retype_password: {
-                            required: "Enter confirm password",
-                            equalTo: "Passwords must match"
-                        },
+                    username: {
+                        required: true,
+                        noSpace: true
                     },
+                    password: {
+                        required: true,
+                        minlength: 6
+                        //maxlength: 8
+                    },
+                    retype_password: {
+                        required: true,
+                        equalTo: "#inputPassword"
+                    },
+                },
+                messages: {
+                    email: {
+                        required: "Enter your email address",
+                        email: "Enter valid email address"
+                    },
+                    username: {
+                        required: "Enter Username",
+
+                    },
+                    password: {
+                        required: "Enter your password",
+                        minlength: "Password must be minimum 6 characters"
+                        //maxlength: "Password must be maximum 8 characters"
+
+                    },
+                    retype_password: {
+                        required: "Enter confirm password",
+                        equalTo: "Passwords must match"
+                    },
+                },
 
 
 
-                    errorPlacement: function(error, element) {
-                        error.hide();
+                errorPlacement: function(error, element) {
+                    error.hide();
+                    $('.messagebox').hide();
+                    error.appendTo($('#alert-message'));
+                    $('.messagebox').slideDown('slow');
+
+
+
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).parents('.form-group').addClass('has-error');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).parents('.form-group').removeClass('has-error');
+                    $(element).parents('.form-group').addClass('has-success');
+                }
+            });
+
+            if ($("#register_form").valid()) {
+                var data1 = $('#register_form').serialize();
+                $.ajax({
+                    type: "POST",
+                    url: "register.php",
+                    data: data1,
+                    success: function(msg) {
+                        console.log(msg);
                         $('.messagebox').hide();
-                        error.appendTo($('#alert-message'));
+                        $('#alert-message').html(msg);
                         $('.messagebox').slideDown('slow');
-
-
-
-                    },
-                    highlight: function(element, errorClass, validClass) {
-                        $(element).parents('.form-group').addClass('has-error');
-                    },
-                    unhighlight: function(element, errorClass, validClass) {
-                        $(element).parents('.form-group').removeClass('has-error');
-                        $(element).parents('.form-group').addClass('has-success');
                     }
                 });
-
-                if ($("#register_form").valid()) {
-                    var data1 = $('#register_form').serialize();
-                    $.ajax({
-                        type: "POST",
-                        url: "register.php",
-                        data: data1,
-                        success: function(msg) {
-                            console.log(msg);
-                            $('.messagebox').hide();
-							$('#alert-message').html(msg);
-							 $('.messagebox').slideDown('slow');
-                        }
-                    });
-                }
-                return false;
-            });
+            }
+            return false;
         });
+    });
     </script>
 </body>
 
