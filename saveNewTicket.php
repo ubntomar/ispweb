9174f198929ed0e6b9bf7a9973426d92aed67891
Omar Hernandez <omar_alberto_h@yahoo.es>
2020-07-24 15:24:09 -0500

    axios saving

--- saveNewTicket.php
+++ saveNewTicket.php
@@ -1,5 +1,24 @@
 <?php
-echo "saved";
+session_start();
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header('Location: login/index.php');
+    exit;
+} else {
+    $user = $_SESSION['username'];
+    $role = $_SESSION["role"];
+}
+include("login/db.php");
+$mysqli = new mysqli($server, $db_user, $db_pwd, $db_name);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
+mysqli_set_charset($mysqli, "utf8");
+date_default_timezone_set('America/Bogota');
+$today = date("Y-m-d");
+$convertdate = date("d-m-Y", strtotime($today));
+$hourMin = date('H:i');
+$debug = 0;
+echo "savxed";
 
 
 
