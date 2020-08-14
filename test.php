0826b4af9634940f24742a1057956a9ec89ce76d
Omar Hernandez <omar_alberto_h@yahoo.es>
2020-08-13 23:15:02 -0500

    test ignore db config

--- test.php
+++ test.php
@@ -1,215 +1,55 @@
 <?php
+//Database configuration   'ispdevinstance-cluster.cluster-cvfeq4s0dnnx.us-east-1.rds.amazonaws.com'
+$server = 'localhost';
+$db_user = 'mikrotik';
+$db_pwd = 'Agwist1.';
+$db_name = 'redesagi_facturacion';//redesagi_facturacion ... .....***55
+$table_name = 'users';
+$table_name_social = 'users_social';
+$table_name_settings= 'admin_settings';
 
-Bien cepilladas por ambos lados ..Boquilleras de 10cmx3mt ....tabletas para hacer inteles.  En venta de maderas.
-1 caja de puntilla de 2 1/2.
+//email configuration
+$from_address = "admin@devlup.com";
+//domain configuration
+$url = "http://projects.devlup.com/LoginSystemv42";
+
+//Admin username
+$admin_user = 'yesy.espejo';
+$admin_password = 'agwist201717';
+
+//strings
+//login
+$msg_pwd_error = 'Password incorrect';
+$msg_un_error = 'Username Doesn\'t exist';
+$msg_email_1 = 'User Account not yet activated.';
+$msg_email_2 = 'Click here to resend activation email';
+
+//Registration form
+$msg_reg_user = 'Username taken.Kindly choose different username';
+$msg_reg_email = 'Email Already registered';
+$msg_reg_activ = 'Activation code has been successfully sent to your Email Address';
+
+//Admin login
+$msg_admin_pwd = 'Incorect password';
+$msg_admin_user = 'Username Doesn\'t exist';
+
+//LOGO text
+$logotxt = "IspExperts";
+
+//Twitter Configuration
+define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
+define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+define('OAUTH_CALLBACK', $url . '/twitter_callback.php');
+
+//Google Configuration
+$Clientid = 'TYPE_CLIENTID_HERE';
+$Email_address = 'TYPE_EMAILADDRESS_HERE';
+$Client_secret = 'TYPE_CLIENT_SECRET_HERE';
+$Redirect_URIs = $url . '/google_connect.php';
+$apikeys = 'TYPE_API_KEYS_HERE';
+
+//facebook configuration
+$fbappid = 'FB_APP_ID';
+$fbsecret = 'FB_SECRET';
 ?>
 
-
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 220 smtp-out.flockmail.com ESMTP Postfix<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: EHLO www.ispexperts.com<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 250-smtp-out.flockmail.com250-PIPELINING250-SIZE 52428800250-VRFY250-ETRN250-STARTTLS250-AUTH LOGIN PLAIN250-ENHANCEDSTATUSCODES250-8BITMIME250 DSN<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: STARTTLS<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 220 2.0.0 Ready to start TLS<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: EHLO www.ispexperts.com<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 250-smtp-out.flockmail.com250-PIPELINING250-SIZE 52428800250-VRFY250-ETRN250-AUTH LOGIN PLAIN250-ENHANCEDSTATUSCODES250-8BITMIME250 DSN<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: AUTH LOGIN<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 334 VXNlcm5hbWU6<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: [credentials hidden]<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 334 UGFzc3dvcmQ6<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: [credentials hidden]<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 235 2.7.0 Authentication successful<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: MAIL FROM:&lt;cliente@ispexperts.com&gt;<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 250 2.1.0 Ok<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: RCPT TO:&lt;omar.a.hernandez.d@gmail.com&gt;<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 250 2.1.5 Ok<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: DATA<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 354 End data with &lt;CR&gt;&lt;LF&gt;.&lt;CR&gt;&lt;LF&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Date: Wed, 5 Aug 2020 10:35:24 -0500<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: To: Cliente Residencial &lt;omar.a.hernandez.d@gmail.com&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: From: =?iso-8859-1?Q?Dpto_T=C3=A8cnico?= &lt;cliente@ispexperts.com&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Reply-To: Dpto de Soporte &lt;cliente@ispexperts.com&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Subject: AG INGENIERIA- Solicitud #254115<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Message-ID: &lt;7yVtjIchcAvZRFnF4nlnWg7v0zbAZuraM2EDXS80Bc@www.ispexperts.com&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: X-Mailer: PHPMailer 6.1.7 (https://github.com/PHPMailer/PHPMailer)<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: MIME-Version: 1.0<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Content-Type: text/html; charset=iso-8859-1<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: Content-Transfer-Encoding: 8bit<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:     &lt;table border=&#039;1&#039; <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 border-radius:3px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 padding:5px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;thead <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 background-color:#111d5e;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 text-align-center;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 border:0px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;th  colspan=&#039;2&#039; <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;h3&gt;Fecha de cierre de Caso #395582: 05-08-2020&lt;/h3&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             text-align:left;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;p&gt;&lt;strong&gt;Titular: &lt;/strong&gt;OMAR&lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;p&gt;&lt;strong&gt;Direcci&ograve;n: &lt;/strong&gt;BRR LA FLORESTA&lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;p&gt;&lt;strong&gt;Tel&egrave;fono: &lt;/strong&gt;3147654655&lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;p&gt;&lt;strong&gt;Fecha apertura de caso: &lt;/strong&gt;2020-08-01&lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;p&gt;&lt;span&gt;ESTADO DEL CASO: &lt;/span&gt;&lt;strong&gt;CERRADO&lt;/strong&gt;&lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/div&gt;    <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;/th&gt; <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;/thead&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;tbody <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             background-color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             color:#000;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;tr <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;td <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;strong <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             background-color:#0275d8;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:6px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             margin:3px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;SOLICITUD&lt;/strong&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;scsadas sasd sa dsdsad sdsads wific&lt;/div&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/td&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;td <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;strong <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             background-color:#0275d8;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:6px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             margin:3px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;SOLUCION&lt;/strong&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;se esta probando el envio de email para notificar cierre de ticket&lt;/div&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/td&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;/tr&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;tr <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;td <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;strong <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             background-color:#0275d8;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:6px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             margin:3px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;RECOMENDACIONES&lt;/strong&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;Mantener Desconectado los equipos&lt;/div&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/td&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;td <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;strong <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             background-color:#0275d8;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:6px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             margin:3px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             color:#fff;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;TECNICO&lt;/strong&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;OMAR&lt;/div&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/td&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;/tr&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;/tbody&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;tfoot&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;tr <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             border:0px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;td <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:              colspan=&#039;2&#039;&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;div <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             style=&#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             padding:10px;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &#039;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                             Direcci&oacute;n de Servicio al Cliente Bogot&aacute; D.C,Colombia<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                             Tel.3134308121-3147654655<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                             &lt;a href=&#039;#&#039;&gt;E-mail:servicioalcliente@ispexperts.com&lt;/a&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                         &lt;/p&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                     &lt;/div&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:                 &lt;/td&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:             &lt;/tr&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:         &lt;/tfoot&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER:     &lt;/table&gt;<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: <br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: .<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 250 2.0.0 Ok: queued as 7A84D120191<br>
-2020-08-05 15:35:24 CLIENT -&gt; SERVER: QUIT<br>
-2020-08-05 15:35:24 SERVER -&gt; CLIENT: 221 2.0.0 Bye<br>
-email errorError sending email
-
-<?php 
-$serverIpAddressArea1="192.168.21.1";
-$serverIpAddressArea2="192.168.30.1";
-$area1Cod="4324"; 
-$area2Cod="4325";
-$vpnUser="agingenieria";
-$vpnPassword="agwist2017"; /////djdfkjghdskfjhl
-?>
