a47f1b1878f1c2e9374938013d34cc879cbe8768
Omar Hernandez Laptop <omar_alberto_h@yahoo.es>
2022-01-12 16:34:07 -0500

    Adjusting cron jobs

--- controller/cron/cron_battery_sensor.php
+++ controller/cron/cron_battery_sensor.php
@@ -1,15 +1,16 @@
 <?php
 $response=exec("curl http://192.168.30.254/50");
-//$response=str_replace("'","\"","{".$response."}");
+//$response=str_replace("'","\"","{".$response."}"); 
 $jArr = json_decode($response,true);
 var_dump($jArr);
 $rele= $jArr["data"]["rele"];
 $batteryVolts=$jArr["data"]["sensor1"];
 $acDcVolt=$jArr["data"]["sensor2"]; 
-
+print "\n El voltage de la Bateria es: $batteryVolts";
 if($rele=="1"){
     if(($batteryVolts<=12.6)){
-        $messageToTelegram="Atencion! Respaldo activo con energia electrica a las baterias en el retiro. Voltaje actual de bateria: $batteryVolts.Voltaje de piloto AC:$acDcVolt  Revisar por favor!!";
+        $res=exec("curl http://192.168.30.254/ON");
+        $messageToTelegram="Atencion! Respaldo activo $res con energia electrica a las baterias en el retiro. Voltaje actual de bateria: $batteryVolts.Voltaje de piloto AC:$acDcVolt  Revisar por favor!!";
         $stringFormatted = preg_replace('/\s+/', '\\t', $messageToTelegram);
         print "mensaje formateado: $stringFormatted";
         $curl="curl -X POST \"https://api.telegram.org/bot896101360:AAFy0Gs_8FET6EZBAEeTJ0qxf-GbLOWQ6Sg/sendMessage\" -d \"chat_id=-340818870&text=$messageToTelegram\"";
