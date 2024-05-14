c5db72dbcea31b10537d0db8c6b09c81c63ea334
ubntomar <omar.a.hernandez.d@gmail.com>
2024-05-14 19:37:06 +0000

    rollback

--- mailer/index.js
+++ mailer/index.js
@@ -4,63 +4,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const fs = require('fs');
-const whatsappClient = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+
 const { sendEmail, sendEmailPayment, sendEmailToCompany, backup, sendPdfFactura } = require('./src/mail');
 
-const SESSION_FILE_PATH = './session.json';
-let sessionData;
+
 
 const app = express();
-const ads = [{ title: 'Hello, world (again)!' }];
-
-// Configurar WhatsApp
-const client = new whatsappClient.Client({
-  puppeteer: { args: ['--no-sandbox'] }
-});
-
-console.log('Cliente de WhatsApp Web inicializado:', client);
-
-let cont=0;
-client.on('qr', (qr) => {
-  qrcode.generate(qr, { small: true });
-  console.log("Generando QR"+(cont++));
-});
-
-client.on('ready', () => {
-  console.log('Client is ready!');
-});
-
-if (fs.existsSync(SESSION_FILE_PATH)) {
-  sessionData = require(SESSION_FILE_PATH);
-  client.loadState(sessionData);
-}
-
-let isAuthenticated = false;
 
 
-client.on('authenticated', (session) => {
-  console.log('Evento autenticado disparado.');
-  if (session) {
-    console.log('Datos de la sesión:', session);
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session, null, 2), (err) => {
-      if (err) {
-        console.error('Error al guardar la sesión:', err);
-      } else {
-        console.log('Sesión guardada exitosamente en', SESSION_FILE_PATH);
-      }
-    });
-  } else {
-    console.log('No hay datos de sesión disponibles.');
-  }
-});
-
-client.on('auth_failure', message => {
-  console.log('Fallo de autenticación:', message);
-});
-
-
-client.initialize();
 
 // Configurar Express
 app.use(helmet());
@@ -123,23 +74,7 @@ app.get('/bk', async (req, res) => {
   }
 });
 
-app.get('/wapi', async (req, res) => {
-  const { phone, message } = req.query;
 
-  if (!phone || !message) {
-    return res.status(400).send({ error: 'Falta el número de teléfono o el mensaje' });
-  }
-
-  const formattedPhone = `${phone}@c.us`;
-
-  try {
-    await client.isRegisteredUser(formattedPhone);
-    await client.sendMessage(formattedPhone, message);
-    res.status(200).send({ success: true });
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
 
 app.listen(3001, () => {
   console.log('listening on port 3001');
